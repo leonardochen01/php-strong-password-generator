713e4c0a8cd63332799d4575c9838ed6c23258f0
leonardochen01 <leonardochen.lc@gmail.com>
2023-05-17 16:53:11 +0200

    added: milestone 1

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+  function generatePassword($length) { //creo funzione, riceve parametro length
+    $list = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!£$%&/()=@#[]{},;.:-_'; //creo una lista di lettere e simboli
+    $password = '';
+
+    for ($i = 0; $i < $length; $i++) { //creo ciclo per ottenere un index casuale dell'array $list finché non arriva al length
+      $randomIndex = rand(0, strlen($list) - 1);
+      $password .= $list[$randomIndex];
+    }
+
+    return $password;
+  }
+
+  if (isset($_GET['length'])) { //controllo che il parametro length sia stato inviato con GET
+    $length = $_GET['length']; 
+    $password = generatePassword($length);
+  }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,6 +37,8 @@
     <input type="submit" value="Genera password" class="mx-5">
 
   </form>
+
+  <h2>La password generata è: <?php echo $password ?></h2>
 
 </body>
 </html>
